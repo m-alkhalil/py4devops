56daaa0bf19a62c3112783b5817b00b7064ab34f
m-alkhalil <146802029+m-alkhalil@users.noreply.github.com>
2025-03-23 08:03:18 +0000

    retrive weather data

--- weather-api/call_weather.py
+++ weather-api/call_weather.py
@@ -15,18 +15,30 @@ api_key = parser.get("WEATHER_API_DATA","api-key")
 payload = {'q': 'Oak Lawn,Il,US','limit':1, 'appid':api_key}
 
 
-#weather_url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={api_key}"
+#
 url = "http://api.openweathermap.org/geo/1.0/direct"#?q=London&limit=5&appid={api_key}
 req = requests.get(url,payload)
 
 # with  open("data.json", "w") as f:
 #         json.dump(req.json(),f,indent=4)
 
-res_dict = {}
-res_dict = req.json()# returns a dictionary         
+#res_dict = {}
+res_dict = req.json()[0] # returns a list         
 print(res_dict)
+print(type(res_dict))
+
+print(res_dict['lon'])
+print(res_dict['lat'])
+
 # print(req.url)
 # print(req.text)
 # print(req.headers)
 #print(api_key)
 
+#### GET WEATHER
+w_payload = {'lat':res_dict['lat'], 'lon':res_dict['lon'], 'appid':api_key}
+weather_url = "https://api.openweathermap.org/data/2.5/weather" #f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={api_key}"
+
+w_req = requests.get (weather_url, w_payload)
+d2 = w_req.json()
+print(type(int(d2['main']['temp'])))
